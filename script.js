a52aee77f040e191d7dfe8a9b73ce456a1ebbc62
Felix Ondullah <felixondullah@gmail.com>
2022-08-14 14:12:30 +0300

    added js and its events

---
+++ script.js
@@ -0,0 +1,6 @@
+function newPicture(){
+    document.getElementById("image").src="../Ondullah-Website/images/ondullah.png";
+}
+function oldPicture(){
+    document.getElementById("image").src="../Ondullah-Website/images/felix-removebg-preview-removebg-preview.png";
+}
